fd66d375b9b7249caf69a9094b989ac9b5fa68a2
Kesavan-B <kesavanui.b@gmail.com>
2023-04-10 12:25:50 +0530

    ddd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Main from './Main';
+import New from './component new/New';
 
 function App() {
   return (
     <div>
       <Main/>
+      <New/>
     </div>
   );
 }
